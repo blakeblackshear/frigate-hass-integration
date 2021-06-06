b1ed5e5a119b0ffdf6e78cfbe79d1ae7dd13b78e
Dermot Duffy <dermot.duffy@gmail.com>
2021-06-06 16:11:46 -0700

    Add testing for device information for binary_sensor.

--- tests/test_binary_sensor.py
+++ tests/test_binary_sensor.py
@@ -6,7 +6,9 @@ from unittest.mock import AsyncMock
 
 from pytest_homeassistant_custom_component.common import async_fire_mqtt_message
 
+from custom_components.frigate.const import DOMAIN, NAME, VERSION
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr, entity_registry as er
 
 from . import (
     TEST_BINARY_SENSOR_FRONT_DOOR_PERSON_MOTION_ENTITY_ID,
@@ -73,3 +75,42 @@ async def test_binary_sensor_api_call_failed(hass: HomeAssistant) -> None:
     client.async_get_stats = AsyncMock(side_effect=Exception)
     await setup_mock_frigate_config_entry(hass, client=client)
     assert not hass.states.get(TEST_BINARY_SENSOR_FRONT_DOOR_PERSON_MOTION_ENTITY_ID)
+
+
+async def test_binary_sensor_device_info(hass: HomeAssistant) -> None:
+    """Verify switch device information."""
+    config_entry = await setup_mock_frigate_config_entry(hass)
+
+    device_registry = dr.async_get(hass)
+    entity_registry = er.async_get(hass)
+
+    device = device_registry.async_get_device(
+        identifiers={(DOMAIN, f"{config_entry.entry_id}:front_door")}
+    )
+    assert device
+    assert device.manufacturer == NAME
+    assert device.model == VERSION
+
+    entities_from_device = [
+        entry.entity_id
+        for entry in er.async_entries_for_device(entity_registry, device.id)
+    ]
+    assert TEST_BINARY_SENSOR_FRONT_DOOR_PERSON_MOTION_ENTITY_ID in entities_from_device
+    assert TEST_BINARY_SENSOR_STEPS_PERSON_MOTION_ENTITY_ID not in entities_from_device
+
+    device = device_registry.async_get_device(
+        identifiers={(DOMAIN, f"{config_entry.entry_id}:steps")}
+    )
+    assert device
+    assert device.manufacturer == NAME
+    assert device.model == VERSION
+
+    entities_from_device = [
+        entry.entity_id
+        for entry in er.async_entries_for_device(entity_registry, device.id)
+    ]
+    assert (
+        TEST_BINARY_SENSOR_FRONT_DOOR_PERSON_MOTION_ENTITY_ID
+        not in entities_from_device
+    )
+    assert TEST_BINARY_SENSOR_STEPS_PERSON_MOTION_ENTITY_ID in entities_from_device
