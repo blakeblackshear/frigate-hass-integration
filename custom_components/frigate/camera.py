7fe2512a63205b278e9dcf65cb77f07aaf5f6e64
Nick Mowen <nickmowen213@gmail.com>
2022-01-12 21:27:15 -0700

    Add state to frigate camera

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -9,7 +9,7 @@ import async_timeout
 from jinja2 import Template
 from yarl import URL
 
-from homeassistant.components.camera import SUPPORT_STREAM, Camera
+from homeassistant.components.camera import SUPPORT_STREAM, STATE_IDLE, STATE_RECORDING, Camera
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_URL
 from homeassistant.core import HomeAssistant, callback
@@ -120,6 +120,13 @@ class FrigateCamera(FrigateEntity, Camera):  # type: ignore[misc]
         if not self._attr_is_streaming:
             return 0
         return cast(int, SUPPORT_STREAM)
+
+    @property
+    def state(self) -> str:
+        """Return the camera state"""
+        if self._camera_config["recordings"]["enabled"]:
+            return STATE_RECORDING
+        return STATE_IDLE
 
     async def async_camera_image(
         self, width: int | None = None, height: int | None = None
