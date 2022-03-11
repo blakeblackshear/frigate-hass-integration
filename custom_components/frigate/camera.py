ac5e8ab1f6fd142d683e6fd9ee653f01520c61e0
Nick Mowen <nickmowen213@gmail.com>
2022-03-11 11:10:37 -0700

    Filter out all from camera

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -46,14 +46,20 @@ async def async_setup_entry(
     config = hass.data[DOMAIN][entry.entry_id][ATTR_CONFIG]
 
     async_add_entities(
-        [
-            FrigateCamera(entry, cam_name, camera_config)
-            for cam_name, camera_config in config["cameras"].items()
-        ]
-        + [
-            FrigateMqttSnapshots(entry, config, cam_name, obj_name)
-            for cam_name, obj_name in get_cameras_and_objects(config)
-        ]
+        filter(
+            lambda camera: not camera.is_all_cam,
+            [
+                FrigateCamera(entry, cam_name, camera_config)
+                for cam_name, camera_config in config["cameras"].items()
+            ],
+        )
+        + filter(
+            lambda snapshot: not snapshot.is_all_cam,
+            [
+                FrigateMqttSnapshots(entry, config, cam_name, obj_name)
+                for cam_name, obj_name in get_cameras_and_objects(config)
+            ],
+        )
     )
 
 
@@ -143,6 +149,10 @@ class FrigateCamera(FrigateEntity, Camera):  # type: ignore[misc]
             return None
         return self._stream_source
 
+    def is_all_cam(self) -> bool:
+        """Return if this snapshot represents the artificial `all`"""
+        return self._cam_name == "all"
+
 
 class FrigateMqttSnapshots(FrigateMQTTEntity, Camera):  # type: ignore[misc]
     """Frigate best camera class."""
@@ -218,3 +228,7 @@ class FrigateMqttSnapshots(FrigateMQTTEntity, Camera):  # type: ignore[misc]
         if self._last_image is None:
             return STATE_IDLE
         return STATE_DETECTED
+
+    def is_all_cam(self) -> bool:
+        """Return if this snapshot represents the artificial `all`"""
+        return self._cam_name == "all"
