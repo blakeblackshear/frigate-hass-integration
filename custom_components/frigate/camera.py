d80679bf34aff2e037c10b6c5e6bb6753b8eaabb
Nicolas Mowen <nickmowen213@gmail.com>
2022-03-27 22:13:22 -0600

    #232: Camera state not updating when toggling recordings (#233)
    * Frigate camera to inherit mqtt entity to get updates to record topic
    
    * Supply frigate_config
    
    * Fix reading of payload property
    
    * Fix tests

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -43,27 +43,42 @@ async def async_setup_entry(
 ) -> None:
     """Camera entry setup."""
 
-    config = hass.data[DOMAIN][entry.entry_id][ATTR_CONFIG]
+    frigate_config = hass.data[DOMAIN][entry.entry_id][ATTR_CONFIG]
 
     async_add_entities(
         [
-            FrigateCamera(entry, cam_name, camera_config)
-            for cam_name, camera_config in config["cameras"].items()
+            FrigateCamera(entry, cam_name, frigate_config, camera_config)
+            for cam_name, camera_config in frigate_config["cameras"].items()
         ]
         + [
-            FrigateMqttSnapshots(entry, config, cam_name, obj_name)
-            for cam_name, obj_name in get_cameras_and_objects(config, False)
+            FrigateMqttSnapshots(entry, frigate_config, cam_name, obj_name)
+            for cam_name, obj_name in get_cameras_and_objects(frigate_config, False)
         ]
     )
 
 
-class FrigateCamera(FrigateEntity, Camera):  # type: ignore[misc]
+class FrigateCamera(FrigateMQTTEntity, Camera):  # type: ignore[misc]
     """Representation a Frigate camera."""
 
     def __init__(
-        self, config_entry: ConfigEntry, cam_name: str, camera_config: dict[str, Any]
+        self,
+        config_entry: ConfigEntry,
+        cam_name: str,
+        frigate_config: dict[str, Any],
+        camera_config: dict[str, Any],
     ) -> None:
         """Initialize a Frigate camera."""
+        super().__init__(
+            config_entry,
+            frigate_config,
+            {
+                "topic": (
+                    f"{frigate_config['mqtt']['topic_prefix']}"
+                    f"/{cam_name}/recordings/state"
+                ),
+                "encoding": None,
+            },
+        )
         FrigateEntity.__init__(self, config_entry)
         Camera.__init__(self)
         self._cam_name = cam_name
@@ -86,6 +101,12 @@ class FrigateCamera(FrigateEntity, Camera):  # type: ignore[misc]
             )
         else:
             self._stream_source = f"rtmp://{URL(self._url).host}/live/{self._cam_name}"
+
+    @callback  # type: ignore[misc]
+    def _state_message_received(self, msg: ReceiveMessage) -> None:
+        """Handle a new received MQTT state message."""
+        self._attr_is_recording = msg.payload.decode("utf-8") == "ON"
+        super()._state_message_received(msg)
 
     @property
     def unique_id(self) -> str:
