605821834a698fdfecbb100a67b4de506fc95fb3
Nick Mowen <nickmowen213@gmail.com>
2022-05-10 21:35:47 -0600

    Cleanup getting config entry

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -3,7 +3,6 @@
 from http import HTTPStatus
 from typing import Any, Dict
 
-from custom_components.frigate.const import DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
@@ -28,8 +27,7 @@ async def _get_diagnostics_for_config_entry(
 async def test_diagnostics(hass: HomeAssistant, hass_client: Any) -> None:
     """Test the diagnostics."""
     # try to get config entry
-    await setup_mock_frigate_config_entry(hass)
-    config_entry: ConfigEntry = hass.config_entries.async_entries(DOMAIN)[0]
+    config_entry: ConfigEntry = await setup_mock_frigate_config_entry(hass)
 
     # test values are as expected
     diagnostic_config: Dict[str, Any] = await _get_diagnostics_for_config_entry(
