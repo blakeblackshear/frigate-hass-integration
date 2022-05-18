a525ed28f40a870e42f7bb14ac8dbfd87f71ce37
Dermot Duffy <dermot.duffy@gmail.com>
2022-05-17 19:26:01 -0700

    Get the tests to pass.

--- tests/test_binary_sensor.py
+++ tests/test_binary_sensor.py
@@ -109,11 +109,16 @@ async def test_binary_sensor_motion_can_be_enabled(hass: HomeAssistant) -> None:
                 hass,
                 dt_util.utcnow() + timedelta(seconds=RELOAD_AFTER_UPDATE_DELAY + 1),
             )
+            await hass.async_block_till_done()
+
+            async_fire_mqtt_message(hass, "frigate/available", "online")
+            await hass.async_block_till_done()
 
             async_fire_mqtt_message(hass, mqtt_topic, "ON")
             await hass.async_block_till_done()
+
             entity_state = hass.states.get(disabled_entity_id)
-            assert entity_state
+            assert entity_state.state == "on"
 
 
 async def test_binary_sensor_api_call_failed(hass: HomeAssistant) -> None:
