ef5bb2b913730a291ff23dd385a772a6e6af0d37
uvjustin <46082645+uvjustin@users.noreply.github.com>
2022-06-17 16:07:21 +1000

    Set use_wallclock_as_timestamps stream option (#283)

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -11,6 +11,7 @@ from yarl import URL
 
 from homeassistant.components.camera import Camera, CameraEntityFeature
 from homeassistant.components.mqtt import async_publish
+from homeassistant.components.stream import CONF_USE_WALLCLOCK_AS_TIMESTAMPS
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_URL
 from homeassistant.core import HomeAssistant, callback
@@ -108,6 +109,8 @@ class FrigateCamera(FrigateMQTTEntity, Camera):  # type: ignore[misc]
         self._set_motion_topic = (
             f"{frigate_config['mqtt']['topic_prefix']}" f"/{self._cam_name}/motion/set"
         )
+        # Set the use_wallclock_as_timestamps stream option
+        self.stream_options[CONF_USE_WALLCLOCK_AS_TIMESTAMPS] = True
 
         streaming_template = config_entry.options.get(
             CONF_RTMP_URL_TEMPLATE, ""
