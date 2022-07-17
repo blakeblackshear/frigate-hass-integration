3c7ccdd18dd8ba6f05463454ab70bb6cc75558da
Nick Mowen <nickmowen213@gmail.com>
2022-07-17 14:55:33 -0600

    Add coral temp sensor

--- custom_components/frigate/sensor.py
+++ custom_components/frigate/sensor.py
@@ -5,7 +5,7 @@ import logging
 from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_URL
+from homeassistant.const import CONF_URL, TEMP_CELSIUS
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -29,6 +29,7 @@ from .const import (
     FPS,
     ICON_CAR,
     ICON_CAT,
+    ICON_CORAL,
     ICON_DOG,
     ICON_OTHER,
     ICON_PERSON,
@@ -57,9 +58,14 @@ async def async_setup_entry(
             for name in value.keys():
                 entities.append(DetectorSpeedSensor(coordinator, entry, name))
         elif key == "service":
-            # Media storage statistics, uptime and Frigate version. For now,
-            # these do not feature in entities.
-            continue
+            # storage & media is not currently supported.
+
+            # setup sensors for coral temp (pcie coral only)
+            temperatures = value.get("temperatures", {})
+
+            if temperatures:
+                for name in temperatures.keys():
+                    entities.append(CoralTempSensor(coordinator, entry, name))
         else:
             entities.extend(
                 [CameraFpsSensor(coordinator, entry, key, t) for t in CAMERA_FPS_TYPES]
@@ -408,3 +414,66 @@ class FrigateObjectCountSensor(FrigateMQTTEntity):
     def icon(self) -> str:
         """Return the icon of the sensor."""
         return self._icon
+
+
+class CoralTempSensor(FrigateEntity, CoordinatorEntity):  # type: ignore[misc]
+    """Frigate Coral Temperature Sensor class."""
+
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_name = "Detection coral temperature"
+
+    def __init__(
+        self,
+        coordinator: FrigateDataUpdateCoordinator,
+        config_entry: ConfigEntry,
+        name: str,
+    ) -> None:
+        """Construct a CoralTempSensor."""
+        self._name = name
+        FrigateEntity.__init__(self, config_entry)
+        CoordinatorEntity.__init__(self, coordinator)
+        self._attr_entity_registry_enabled_default = False
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID to use for this entity."""
+        return get_frigate_entity_unique_id(
+            self._config_entry.entry_id, "sensor_temp", self._name
+        )
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Get device information."""
+        return {
+            "identifiers": {get_frigate_device_identifier(self._config_entry)},
+            "name": NAME,
+            "model": self._get_model(),
+            "configuration_url": self._config_entry.data.get(CONF_URL),
+            "manufacturer": NAME,
+        }
+
+    @property
+    def state(self) -> float | None:
+        """Return the state of the sensor."""
+        if self.coordinator.data:
+            data = (
+                self.coordinator.data.get("services", {})
+                .get("temperatures", {})
+                .get(self._name, 0.0)
+            )
+            if data is not None:
+                try:
+                    return float(data)
+                except ValueError:
+                    pass
+        return None
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement of the sensor."""
+        return TEMP_CELSIUS
+
+    @property
+    def icon(self) -> str:
+        """Return the icon of the sensor."""
+        return ICON_CORAL
