72f03c50bfcb868100418edf9af361708e5c49ec
Nick Mowen <nickmowen213@gmail.com>
2022-12-21 09:15:15 -0700

    Fix logic

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -67,9 +67,9 @@ async def async_setup_entry(
             FrigateMqttSnapshots(entry, frigate_config, cam_name, obj_name)
             for cam_name, obj_name in get_cameras_and_objects(frigate_config, False)
         ]
-        + [BirdseyeCamera(entry, frigate_client)]
+        + ([BirdseyeCamera(entry, frigate_client)]
         if frigate_config.get("restream", {}).get("birdseye", False)
-        else []
+        else [])
     )
 
     # setup services
