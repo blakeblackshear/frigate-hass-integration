bd63a2c893fe168b197d38c6102bc07cd79d27fe
Felipe Santos <felipecassiors@gmail.com>
2023-01-25 09:21:51 -0300

    Add reminder to remove after fix

--- custom_components/frigate/camera.py
+++ custom_components/frigate/camera.py
@@ -167,6 +167,7 @@ class FrigateCamera(FrigateMQTTEntity, Camera):  # type: ignore[misc]
                     **self._camera_config
                 )
             else:
+                # TODO: Remover after https://github.com/AlexxIT/go2rtc/issues/194 gets fixed
                 self._stream_source = f"rtsp://{URL(self._url).host}:8554/{self._cam_name}?video=copy&audio=aac"
 
         elif self._camera_config.get("rtmp", {}).get("enabled"):
