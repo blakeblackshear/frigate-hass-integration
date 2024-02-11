5cbb9ec29172f3e355055a254e133c6b638a70ad
Nicolas Mowen <nickmowen213@gmail.com>
2024-02-11 06:56:20 -0700

    Proxy gif notification requests

--- custom_components/frigate/views.py
+++ custom_components/frigate/views.py
@@ -276,6 +276,9 @@ class NotificationsProxyView(ProxyView):
 
         if path.endswith("clip.mp4"):
             return f"api/events/{event_id}/clip.mp4"
+
+        if path.endswith("preview.gif"):
+            return f"api/events/{event_id}/preview.gif"
         return None
 
     def _permit_request(
