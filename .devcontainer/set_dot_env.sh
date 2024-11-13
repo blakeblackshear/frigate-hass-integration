80d1080c3f9aca230665087ddd628856a6a15008
Brenan Kelley <brenankly@gmail.com>
2024-11-13 16:42:05 +0000

    fix: cross platform compatibility of set_dot_env.sh

--- .devcontainer/set_dot_env.sh
+++ .devcontainer/set_dot_env.sh
@@ -8,7 +8,7 @@ readonly file=".env"
 
 echo "Writing ${wanted_line} to ${file}" >&2
 if [[ -f "${file}" ]] && grep --quiet "^${wanted_line_key}=" "${file}"; then
-    sed --in-place "s,^${wanted_line_key}=.*,${wanted_line}," "${file}"
+    sed "s,^${wanted_line_key}=.*,${wanted_line}," "${file}"
 else
     echo "${wanted_line}" >>"${file}"
 fi
