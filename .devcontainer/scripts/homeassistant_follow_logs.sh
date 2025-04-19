6d0b7a38f878715da5a686f0c682ce5605f8cc2e
Felipe Santos <felipecassiors@gmail.com>
2025-04-19 18:56:21 +0000

    Revamp devcontainer

--- .devcontainer/scripts/homeassistant_follow_logs.sh
+++ .devcontainer/scripts/homeassistant_follow_logs.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 # This ensures the container is up and, it
 # won't do anything if it is already up.
-docker compose up --detach hass
+docker compose up --detach homeassistant
 
 # Use docker logs instead of docker compose logs because the latter
 # never exits, even after container is stopped.
-container_id=$(docker compose ps hass --quiet)
+container_id=$(docker compose ps homeassistant --quiet)
 
 # Only get the logs since the last time the container was started, so
 # that we don't confuse VS Code's problem matcher.
